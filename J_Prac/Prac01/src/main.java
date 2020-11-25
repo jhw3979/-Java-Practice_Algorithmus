81b867c03bb3c7ea88ecb9a5ceae3e128d8a7a23
진현우 <49802982+jhw3979@users.noreply.github.com>
2020-11-25 16:33:35 +0900

    연습중

---
+++ J_Prac/Prac01/src/main.java
@@ -0,0 +1,34 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("---------------------------");
+        System.out.println("출력 연습");
+        System.out.println("helloworld");
+        System.out.println("문자열 출력");
+        //System.out.println('문자열');
+        System.out.println("문자열\"안녕하세요\"");
+        System.out.println("이스케이프\n문자연습\n입니다.");
+        System.out.println("---------------------------");
+        System.out.println("변수 연습");
+        int a = 10;
+        double b = 10.1;
+        String c = "c";
+        System.out.println("int 변수 a : " + a);
+        System.out.println("double 변수 b : " + b);
+        System.out.println("String 클래스 c : " + c);
+        short d = 100;
+        byte e = 100;
+        System.out.println("short 변수 d : " + d);
+        System.out.println("byte 변수 e :" + e);
+        float f = 1.1F;
+        long g = 1122L;
+        System.out.println("float 변수 f : " + f);
+        System.out.println("long 변수 g :" + g);
+        System.out.println("---------------------------");
+        System.out.println("명시적 형변환 해보기");
+        int h = 45;
+        System.out.println("int 변수 h : " + h);
+        System.out.println("형변환 문자 h : "+ (char)h);
+        System.out.println("---------------------------");
+        
+    }
+}
