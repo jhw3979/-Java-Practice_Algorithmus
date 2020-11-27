dfa5532654f9762359d4c4c0ff84751fcaf3c1ff
진현우 <49802982+jhw3979@users.noreply.github.com>
2020-11-27 23:53:32 +0900

    practice

---
+++ J_Prac/Prac01/src/prac/basic/operator.java
@@ -0,0 +1,7 @@
+package prac.basic;
+
+public class operator {
+    int a = 1;
+    System.out.println(++a);
+    System.out.println(a++);
+}
---
+++ J_Prac/Prac01/src/prac_main.java
@@ -0,0 +1,8 @@
+import prac.basic.operator;
+
+public class prac_main {
+    public static void main(String[] args){
+        System.out.println("helloworld");
+        operator();
+    }
+}
