104e9b3b55d877dcc792d5446a359e11ef44af59
진현우 <49802982+jhw3979@users.noreply.github.com>
2020-12-01 23:24:06 +0900

    객체 연습중
    생성자등

--- src/main.java
+++ src/main.java
@@ -7,10 +7,6 @@ public class main {
     public static void main(String[] args) {
         //패키지 사용
         deepening a = new deepening();
-        a.class_variable_prac();
-        a.a = 0;
-        a.class_variable_prac();
-        deepening b = new deepening();
-        b.class_variable_prac();
+        a.inheritance();
     }
 }
--- src/prac/deepening.java
+++ src/prac/deepening.java
@@ -1,9 +1,40 @@
 package prac;
 
+class constructor_prac{
+    int a, b;
+
+    public void setvalue(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public void sum() {
+        System.out.printf("%d + %d = %d\n", a, b, a + b);
+    }
+}
+
+class SubstractionableCalculator extends constructor_prac {
+    public void substract() {
+        System.out.printf("%d - %d = %d\n", a, b, a - b);
+    }
+}
 public class deepening {
     public static int a = 1000;
 
     public void class_variable_prac(){
         System.out.println(deepening.a);
+    }
+
+    public void class_constructor_prac() {
+        constructor_prac a = new constructor_prac();
+        a.setvalue(10, 30);
+        a.sum();
+    }
+
+    public void inheritance(){
+        SubstractionableCalculator a = new SubstractionableCalculator();
+        a.setvalue(50, 100);
+        a.sum();
+        a.substract();
     }
 }
