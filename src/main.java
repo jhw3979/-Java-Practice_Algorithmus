f52b92b0c955443b6512373e4c3ae4124833174d
진현우 <49802982+jhw3979@users.noreply.github.com>
2020-12-19 23:39:59 +0900

    제네릭

--- src/main.java
+++ src/main.java
@@ -2,8 +2,7 @@ import prac.*;
 
 public class main {
     public static void main(String[] args) {
-        //패키지 사용
-        GenericDemo<String> a = new GenericDemo<String>();
-        GenericDemo<StringBuilder> b = new GenericDemo<StringBuilder>();
+        GenericDemo a = new GenericDemo();
+        a.run();
     }
 }
--- src/prac/GenericDemo.java
+++ src/prac/GenericDemo.java
@@ -1,5 +1,21 @@
 package prac;
 
-public class GenericDemo<T> {
+class StudentInfo{
+    public int grade;
+    StudentInfo(int grade){ this.grade = grade; }
+}
+class EmployeeInfo{
+    public int rank;
+    EmployeeInfo(int rank){ this.rank = rank; }
+}
+class Person<T>{
     public T info;
+    Person(T info){ this.info = info; }
+}
+public class GenericDemo{
+    public void run() {
+        Person<EmployeeInfo> p1 = new Person<EmployeeInfo>(new EmployeeInfo(1));
+        EmployeeInfo ei1 = p1.info;
+        System.out.println(ei1.rank); // 성공
+    }
 }
